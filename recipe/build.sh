24548e5ee9344ef3abee1db8e17e4b78c761d59c
Daniel Nachun <daniel.nachun@gmail.com>
2025-02-01 14:29:00 -0800

    fix build

--- recipe/build.sh
+++ recipe/build.sh
@@ -5,6 +5,7 @@ set -o xtrace -o nounset -o pipefail -o errexit
 mkdir build -p
 cd build 
 
+export FFLAGS="${FFLAGS} -fallow-argument-mismatch"
 cmake -D CMAKE_BUILD_TYPE:STRING=Release \
       -D CMAKE_PREFIX_PATH:FILEPATH=${PREFIX} \
       -D CMAKE_INSTALL_PREFIX:FILEPATH=${PREFIX} \
